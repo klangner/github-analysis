b366f43f04e25284776f7e547489914293bbb0a5
Krzysztof Langner <klangner@gmail.com>
2012-11-21 20:56:52 +0100

    Try to parse json to find end of record

--- src/main/java/com/matrobot/gha/dataset/FileDatasetReader.java
+++ src/main/java/com/matrobot/gha/dataset/FileDatasetReader.java
@@ -1,6 +1,5 @@
 package com.matrobot.gha.dataset;
 
-import java.io.BufferedReader;
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStream;
@@ -41,7 +40,7 @@ import com.google.gson.JsonSyntaxException;
  */
 public class FileDatasetReader implements IDatasetReader{
 
-	private BufferedReader reader;
+	private String fileContents;
 	private Gson gson = new Gson();
 	private String filename;
 	
@@ -66,8 +65,18 @@ public class FileDatasetReader implements IDatasetReader{
 	public FileDatasetReader(InputStream inputStream) throws IOException{
 
 		InputStream gzipStream = new GZIPInputStream(inputStream);
-		Reader decoder = new InputStreamReader(gzipStream, "UTF-8");
-		reader = new BufferedReader(decoder);
+		Reader reader = new InputStreamReader(gzipStream, "UTF-8");
+		char[] buffer = new char[100000];
+		int count;
+		
+		StringBuilder contents = new StringBuilder();
+		while((count=reader.read(buffer)) > 0){
+			contents.append(buffer, 0, count);
+		}
+		fileContents = contents.toString();
+		if(fileContents.indexOf("\n") == -1){
+			fileContents = fileContents.replace("}{", "}\n{");
+		}
 	}
 
 
@@ -76,15 +85,16 @@ public class FileDatasetReader implements IDatasetReader{
 		DataRecord data = null;
 		
 		try {
-			String line = reader.readLine();
+			String line = readLine();
 			if(line != null){
 				try{
 					data = gson.fromJson(line, DataRecord.class);
 				}catch(JsonSyntaxException e){
 					System.err.println(e);
 					System.err.println(filename);
+//					System.out.println("Length:" + line.length());
 					// try next record
-					data = readNextRecord();
+//					data = readNextRecord();
 				}
 			}
 		} catch (IOException e) {
@@ -93,4 +103,72 @@ public class FileDatasetReader implements IDatasetReader{
 		
 		return data;
 	}
+
+
+	private String readLine() throws IOException {
+		
+		String line = null;
+		
+		if(fileContents.length() > 0){
+			int index = fileContents.indexOf("\n");
+			if(index > 0){
+				line = fileContents.substring(0, index);
+				fileContents = fileContents.substring(index).trim();
+			}
+			else{
+				line = fileContents;
+				fileContents = "";
+			}
+		}
+		
+		return line;
+	}
+	
+	private String readLine2() throws IOException {
+		
+		String line = null;
+		
+		for(int i = 0; i < fileContents.length(); i++){
+			if(fileContents.charAt(i) == '{'){
+				if(i > 0){
+					fileContents = fileContents.substring(i);
+					System.out.println(">" + fileContents.substring(0, 50).trim());
+					
+				}
+				break;
+			}
+		}
+		
+		int counter = 0;
+		int lastChar = ' ';
+		boolean isString = false;
+		for(int i = 0; i < fileContents.length(); i++){
+			if(fileContents.charAt(i) == '{'){
+				if(!isString){
+					counter ++;
+				}
+			}
+			else if(fileContents.charAt(i) == '}'){
+				if(!isString){
+					counter --;
+					if( counter == 0){
+						line = fileContents.substring(0, i+1);
+						fileContents = fileContents.substring(i+1);
+						break;
+					}
+				}
+			}
+			else if(fileContents.charAt(i) == '\"' && lastChar != '\\'){
+				isString = !isString;
+			}
+			
+			lastChar = fileContents.charAt(i);
+		}
+
+		System.out.println(line);
+		return line;
+	}
+	
 }
+
+
