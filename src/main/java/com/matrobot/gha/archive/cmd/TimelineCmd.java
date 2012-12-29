99ab91988f654b55a25d513682e577ad2169bd3d
Krzysztof Langner <klangner@gmail.com>
2012-12-29 15:54:46 +0100

    Added timeline

---
+++ src/main/java/com/matrobot/gha/archive/cmd/TimelineCmd.java
@@ -0,0 +1,108 @@
+package com.matrobot.gha.archive.cmd;
+
+import java.io.IOException;
+import java.io.PrintStream;
+import java.util.HashSet;
+import java.util.Set;
+
+import com.matrobot.gha.Configuration;
+import com.matrobot.gha.ICommand;
+import com.matrobot.gha.archive.event.EventReader;
+import com.matrobot.gha.archive.event.EventRecord;
+import com.matrobot.gha.archive.event.IEventReader;
+
+/**
+ * Find repository state by going back in history events.
+ * E.g.
+ * Find number of forks in 2011-2 by counting back from 
+ * 
+ * @author Krzysztof Langner
+ */
+public class TimelineCmd implements ICommand{
+
+	class Record{
+		String date;
+		int newUsers = 0;
+		Set<String> activeUsers = new HashSet<String>();
+	}
+	
+	private Set<String> allUsers = new HashSet<String>();
+	private Record currentRecord = null;
+	private int index = 1;
+	private PrintStream outputStream;
+	
+	
+	@Override
+	public void run(Configuration params) throws IOException {
+
+		initOutputStream(params);
+		IEventReader reader;
+		reader = new EventReader(params.getMonthFolders());
+		parse(reader);
+	}
+
+
+	private void initOutputStream(Configuration params) {
+		outputStream = params.getOutputStream(); 
+		outputStream.println("index,date,nu,au");
+	}
+
+
+	private void parse(IEventReader reader) {
+		
+		EventRecord	event;
+		while((event = reader.next()) != null){
+			prepareCurrentRecord(event);
+			String actor = event.getActorLogin();
+			if(actor != null){
+				currentRecord.activeUsers.add(actor);
+				if(!allUsers.contains(actor)){
+					currentRecord.newUsers ++;
+					allUsers.add(actor);
+				}
+			}
+		}
+		
+		saveRecord(currentRecord);
+	}
+
+
+	private void prepareCurrentRecord(EventRecord event) {
+
+		String date = event.created_at.substring(0, 7);
+		
+		if(currentRecord == null){
+			currentRecord = new Record();
+			currentRecord.date = date;
+		}
+		else if(!currentRecord.date.equals(date)){
+			System.out.println(date);
+			saveRecord(currentRecord);
+			currentRecord = new Record();
+			currentRecord.date = date;
+		}
+	}
+
+
+	private void saveRecord(Record record){
+		outputStream.println(
+			index+","+record.date+","+record.newUsers+","+record.activeUsers.size()
+		);
+		index++;
+	}
+
+	
+	/**
+	 * for local testing
+	 * @param args
+	 * @throws IOException
+	 */
+	public static void main(String[] args) throws IOException {
+
+		Configuration params = new Configuration("configs/timeline.yaml");
+		
+		TimelineCmd app = new TimelineCmd();
+		app.run(params);
+	}
+	
+}
